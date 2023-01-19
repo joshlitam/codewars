768f2a40966e09d42814f8fd330a586da9bc269b
Joshua Litam <joshua_litam@yahoo.com>
2023-01-19 11:36:43 +0800

    added codewars and homework for January 19, 2023

--- templated-folder/js/main.js
+++ templated-folder/js/main.js
@@ -1,1 +1,6 @@
+// The JS Way Exercises
+
+const documentName = 'Templated Folder'
+document.querySelector('h1').innerText += ` This document is for the ${documentName}. Please see the console log for details`
+
 console.log('Hello, World!')
