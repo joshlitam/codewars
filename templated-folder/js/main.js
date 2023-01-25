71f12b0fc3f9feffe70304fe5bdcc528d2f83825
Joshua Nikolai Litam <74515811+joshlitam@users.noreply.github.com>
2023-01-25 14:38:36 +0800

    Delete templated-folder directory

--- templated-folder/js/main.js
+++
@@ -1,6 +0,0 @@
-// The JS Way Exercises
-
-const documentName = 'Templated Folder'
-document.querySelector('h1').innerText += ` This document is for the ${documentName}. Please see the console log for details`
-
-console.log('Hello, World!')
