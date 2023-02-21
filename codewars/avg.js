c0582eac8b14b5854dd54aab67d740200cd41858
Joshua Litam <joshua_litam@yahoo.com>
2023-02-21 12:22:43 +0800

    shortest function to average an array

---
+++ codewars/avg.js
@@ -0,0 +1,12 @@
+/*codewar challenge for the day link: https://www.codewars.com/kata/56c22cdbe0c0f7cae2001789/train/javascript
+7 kyu: Write shortest function to calculate Average number of Array
+Given an array of integers, calculate the Average of these numbers.
+
+Main challenge is to write shortest and compact function for it.
+
+For example: var a = [0, 1, 2];
+avg(a) // output should be 1
+Output of function will be also checked in tests, however most important is to write the shortest possible function (code length < 100). Input will always be valid.
+*/
+
+const avg = a => a.reduce((a, b) => a + b)/a.length
