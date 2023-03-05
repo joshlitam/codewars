1e1fd123b04d1bfb1d66bd76ac2566bba55b00de
Joshua Litam <joshua_litam@yahoo.com>
2023-03-05 12:00:33 +0800

    recycle me

---
+++ codewars/recycleMe.js
@@ -0,0 +1,19 @@
+/*codewar challenge for the day link: https://www.codewars.com/kata/59fb783bab11f89202001083/train/javascript
+7 kyu: Recycle
+
+You are having a BBQ, after the BBQ you are left with the rubbish. You have 3 recycling boxes:
+
+Red: Plastics, Green: Glass, Blue: Card.
+
+You will need to sort the rubbish according to the material and return the number of items in each box in the form of an array i.e [2,3,4] where 2 is the number of plastic items, 3 is the number of glass items and 4 is the number of card items. assume:
+
+Plastics > 0, Glass < 0, Card = 0
+*/
+
+function recycleMe(recycle){ 
+  
+  let numOfPlastics = recycle.filter(x => x > 0).length
+  let numOfGlass = recycle.filter(x => x < 0).length
+  let numOfCard = recycle.filter(x => x == 0).length
+  return [numOfPlastics, numOfGlass, numOfCard]
+}
