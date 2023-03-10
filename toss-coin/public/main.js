384028f6a8e60a342dd68b482846c707996207ba
Joshua Nikolai Litam <74515811+joshlitam@users.noreply.github.com>
2023-03-10 09:57:43 +0800

    Delete toss-coin directory

--- toss-coin/public/main.js
+++
@@ -1,6 +0,0 @@
-const tossCoin = document.querySelector('#toss-button').addEventListener('click', _ => {
-    let randomNum = Math.round(Math.random())
-    let result = randomNum ? 'heads' : 'tails'
-    console.log(randomNum)
-    document.querySelector('h1').innerText = result
-})
