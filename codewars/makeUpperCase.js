ec426087b0d1d2021e9214d5bc52ceebbdc683b2
Joshua Litam <joshua_litam@yahoo.com>
2023-03-12 17:45:45 +0800

    make upper case

---
+++ codewars/makeUpperCase.js
@@ -0,0 +1,9 @@
+/*codewar challenge for the day link: https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7/train/javascript
+8 Kyu: makeUpperCase
+
+Write a function which converts the input string to uppercase.
+*/
+
+function makeUpperCase(str) {
+    return str.toUpperCase()
+  }
