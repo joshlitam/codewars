cc06f8adb4f4516cd77466e6713f90173f9e3fa8
Joshua Litam <joshua_litam@yahoo.com>
2023-03-18 15:46:56 +0800

    type of sum

---
+++ codewars/typeOfSum.js
@@ -0,0 +1,9 @@
+/*codewar challenge for the day link: https://www.codewars.com/kata/5a2e9ae2b6cfd7692a0000ba/train/javascript
+8 Kyu: Type of Sum
+
+Return the type of the sum of the two arguments
+*/
+
+function typeOfSum(a, b) {
+    return typeof(a + b)
+    }
