63f3f0180dca0155fb3ba1b86683084b4aa52c33
Joshua Litam <joshua_litam@yahoo.com>
2023-03-30 14:08:35 +0800

    words to sentence

---
+++ codewars/wordsToSentence.js
@@ -0,0 +1,12 @@
+/*codewar challenge for the day link: https://www.codewars.com/kata/57a06005cf1fa5fbd5000216/train/javascript
+7 kyu: Words to Sentence
+Write function which will create a string from a list of strings, separated by space.
+
+Example:
+
+["hello", "world"] -> "hello world"
+*/
+
+function wordsToSentence(words) {
+    return words.join(" ")
+  }
