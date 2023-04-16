6bc9095f17bfe6f6bad2ceca607dc54d3e86344e
Joshua Litam <joshua_litam@yahoo.com>
2023-04-16 17:45:32 +0800

    length and two values

---
+++ codewars/alternate.js
@@ -0,0 +1,21 @@
+/*codewar challenge for the day link: https://www.codewars.com/kata/62a611067274990047f431a8/train/javascript
+7 kyu: Lengths and two values
+Given an integer n and two other values, build an array of size n filled with these two values alternating.
+
+Examples
+5, true, false     -->  [true, false, true, false, true]
+10, "blue", "red"  -->  ["blue", "red", "blue", "red", "blue", "red", "blue", "red", "blue", "red"]
+0, "one", "two"    -->  []
+*/
+
+function alternate(n, firstValue, secondValue){
+    let arr = []
+    for (i = 0; i < n; ++i) {
+      if (i % 2 !== 0) {
+        arr.push(secondValue)
+      } else {
+        arr.push(firstValue)
+      }
+    }
+  return arr
+  }
