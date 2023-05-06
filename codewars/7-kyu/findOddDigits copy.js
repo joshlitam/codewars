12f0c75ea3175ebc50541ec951d34e0c101b355f
Joshua Litam <joshua_litam@yahoo.com>
2023-05-06 15:28:58 +0800

    find first k odd numbers

--- codewars/7-kyu/findOddDigits copy.js
+++ codewars/7-kyu/findOddDigits copy.js
@@ -1,17 +1,19 @@
-/*codewar challenge for the day link: https://www.codewars.com/kata/5667e8f4e3f572a8f2000039/train/javascript
-7 kyu: Mumbling
-This time no story, no theory. The examples below show you how to write function accum:
+/*codewar challenge for the day link: https://www.codewars.com/kata/59e8afdd0863c7bcb300013a/train/javascript
+7 kyu: Give me the k first digits. Odd digits.
+You're given two integers – n and k. n is always positive, while k >= 0. Write a function that returns first k odd digits in a number n.
 
-Examples:
-accum("abcd") -> "A-Bb-Ccc-Dddd"
-accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
-accum("cwAt") -> "C-Ww-Aaa-Tttt"
+The output should also be an integer.
 
-The parameter of accum is a string which includes only letters from a..z and A..Z.
+In the following cases a function should return 0:
+
+there are no odd digits in a number n;
+k is bigger than a number of digits in n;
+k = 0;
+k is bigger than a number of odd digits in n.
+BTW, is 0 an odd number or an even one?
 */
 
-// seperate characters in a string and repeat them based on index, joined by a hyphen
-const accum = s => s.split("").map((x, i) => (x.toUpperCase() + x.toLowerCase().repeat(i))).join("-")
-
-// application
-console.log(accum("AzmfasS"))
+function findOddDigits(n, k) {
+    let odds = n.toString().split("").filter(x => x % 2 !== 0)
+    return odds.length >= k ? Number(odds.slice(0, k).join("")) : 0
+  }
