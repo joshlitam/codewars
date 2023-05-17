fdf42d31da9f21c79a6031c34ac99e9eca352069
Joshua Litam <joshua_litam@yahoo.com>
2023-05-17 09:33:52 +0800

    remove method in arrays

---
+++ codewars/7-kyu/remove.js
@@ -0,0 +1,19 @@
+/*codewar challenge for the day link: https://www.codewars.com/kata/565d76490397dcf0e700001e/train/javascript
+7 kyu: Remove method in Arrays
+Some people really wonder why JavaScript arrays don't have a remove function to remove an element.
+
+Go ahead and make one.
+
+var someArray = [1, 2, 3];
+someArray.remove(1);
+This should remove the element on index 1 making someArray = [1, 3]
+
+You also need to handle invalid inputs: If index is not a number or out of bounds, return the array unchanged.
+
+
+*/
+
+Array.prototype.remove = function(index) {
+    index > 0 ? this.splice(index, 1) : this
+    return this
+  };
