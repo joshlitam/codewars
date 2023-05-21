7b7c2155cf65c90790fe33bcff628f7fa8bdc5d2
Joshua Litam <joshua_litam@yahoo.com>
2023-05-21 09:55:58 +0800

    configure package json for node app

---
+++ codewars/configuration.js
@@ -0,0 +1,15 @@
+/*codewar challenge for the day link: https://www.codewars.com/kata/5692a582cc22387ccf000017/train/javascript
+8 Kyu: Configure package json for a node application
+
+Create a package.json configuration and assign it to the configuration constant so it can be tested.
+
+To pass the kata you need only the least that a real package file would require.
+*/
+
+const configuration = {
+    "name": "your-pack-name",
+    "version": "0.1.0",
+    "description": ""
+  };
+  // Hint: try creating with `npm init` command
+  // Help! -> https://docs.npmjs.com/files/package.json
