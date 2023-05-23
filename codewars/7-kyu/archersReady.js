f6094311547739ba95d040687ae8394d371223cf
Joshua Litam <joshua_litam@yahoo.com>
2023-05-23 12:45:08 +0800

    every archer has its arrows

---
+++ codewars/7-kyu/archersReady.js
@@ -0,0 +1,21 @@
+/*codewar challenge for the day link: https://www.codewars.com/kata/559f89598c0d6c9b31000125/train/javascript
+7 kyu: Every archer has its arrows
+#It's show time! Archers have gathered from all around the world to participate in the Arrow Function Faire. But the faire will only start if there are archers signed and if they all have enough arrows in their quivers - at least 5 is the requirement! Are all the archers ready?
+
+#Reference https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+
+#Argument archers is an array of integers, in which each element corresponds to the number of arrows each archer has.
+
+#Return Your function must return true if the requirements are met or false otherwise.
+
+#Examples archersReady([1, 2, 3, 4, 5, 6, 7, 8]) returns false because there are archers with not enough arrows.
+
+archersReady([5, 6, 7, 8]) returns true.
+
+
+*/
+
+function archersReady(archers){
+    // arrow it
+    return archers.length > 0 ? archers.every(x => x >= 5) : false
+  }
