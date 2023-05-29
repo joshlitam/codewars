5a54742aff0c1fcb62ff8d1c3da1815724ae3f6e
Joshua Litam <joshua_litam@yahoo.com>
2023-05-29 09:41:57 +0800

    the mean of two means

---
+++ codewars/getMean.js
@@ -0,0 +1,22 @@
+/*codewar challenge for the day link: https://www.codewars.com/kata/583df40bf30065fa9900010c/train/javascript
+7 kyu: The mean of two means
+Write a function getMean that takes as parameters an array (arr) and 2 integers (x and y). The function should return the mean between the mean of the the first x elements of the array and the mean of the last y elements of the array.
+
+The mean should be computed if both x and y have values higher than 1 but less or equal to the array's length. Otherwise the function should return -1.
+
+getMean([1,3,2,4], 2, 3) should return 2.5 because: the mean of the the first 2 elements of the array is (1+3)/2=2 and the mean of the last 3 elements of the array is (4+2+3)/3=3 so the mean of those 2 means is (2+3)/2=2.5.
+
+getMean([1,3,2,4], 1, 2) should return -1 because x is not higher than 1.
+
+getMean([1,3,2,4], 2, 8) should return -1 because 8 is higher than the array's length.
+*/
+
+function getMean(arr, x, y) {
+    if (x > 1 && y > 1 && x <= arr.length && y <= arr.length) {
+      const firstMean = (arr.slice(0, x).reduce((a, b) => a + b, 0)/x)
+      const secondMean = (arr.slice(arr.length - y, arr.length).reduce((a, b) => a + b, 0)/y)
+      return (firstMean + secondMean) / 2
+    } else {
+      return -1
+    }
+  }
