087bdefbcb85cbe9f9662c6b043fd9754de93b8d
Joshua Litam <joshua_litam@yahoo.com>
2023-06-01 16:01:22 +0800

    boolean logic from scratch

--- codewars/xor.js
+++ codewars/xor.js
@@ -1,17 +1,26 @@
-/*codewar challenge for the day link: https://www.codewars.com/kata/5667e8f4e3f572a8f2000039/train/javascript
-7 kyu: Mumbling
-This time no story, no theory. The examples below show you how to write function accum:
+/*codewar challenge for the day link: https://www.codewars.com/kata/584d2c19766c2b2f6a00004f/train/javascript
+7 kyu: Boolean Logic from Scratch
+Task
+You need to implement two functions, xor and or, that replicate the behaviour of their respective operators:
 
-Examples:
-accum("abcd") -> "A-Bb-Ccc-Dddd"
-accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
-accum("cwAt") -> "C-Ww-Aaa-Tttt"
+xor = Takes 2 values and returns true if, and only if, one of them is truthy.
+or = Takes 2 values and returns true if either one of them is truthy.
+When doing so, you cannot use the or operator: ||.
 
-The parameter of accum is a string which includes only letters from a..z and A..Z.
+Input
+Not all input will be booleans - there will be truthy and falsey values [the latter including also empty strings and empty arrays]
+There will always be 2 values provided
 */
 
-// seperate characters in a string and repeat them based on index, joined by a hyphen
-const accum = s => s.split("").map((x, i) => (x.toUpperCase() + x.toLowerCase().repeat(i))).join("-")
-
-// application
-console.log(accum("AzmfasS"))
+const or = (a, b) => {
+    if (Boolean(a) !== Boolean(b)) {
+      return Boolean(a) ? true : Boolean(b)
+    } else {
+      return Boolean(a)
+    }
+  };
+  
+  const xor = (a, b) => {
+    return Boolean(a) === Boolean(b) ? false : true 
+    
+  };
