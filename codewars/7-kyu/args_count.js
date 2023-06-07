aebdc6c5ab777fac41a0bd3e6ebedafa26e3000d
Joshua Litam <joshua_litam@yahoo.com>
2023-06-07 08:59:30 +0800

    how many arguments

---
+++ codewars/7-kyu/args_count.js
@@ -0,0 +1,9 @@
+/*codewar challenge for the day link: https://www.codewars.com/kata/5c44b0b200ce187106452139/train/javascript
+7 kyu: How many arguments
+args_count(1, 2, 3) -> 3
+args_count(1, 2, 3, 10) -> 4
+*/
+
+function args_count () {
+    return arguments.length
+  }
