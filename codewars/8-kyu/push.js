4881f54b6c61854d3e71bd8ac02a06513443cfb1
Joshua Litam <joshua_litam@yahoo.com>
2023-06-09 20:11:45 +0800

    add item to an array

---
+++ codewars/8-kyu/push.js
@@ -0,0 +1,12 @@
+/*codewar challenge for the day link: https://www.codewars.com/kata/511f0fe64ae8683297000001/train/javascript
+8 Kyu: Basic Training: Add item to an Array
+
+Add the value "codewars" to the websites array.
+After your code executes the websites array should == ["codewars"]
+
+The websites array has already been defined for you using the following code:
+
+var websites = [];
+*/
+
+websites.push('codewars')
