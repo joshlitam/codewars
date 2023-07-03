69678a0148a36fc6c4c195df4444d9b888cd81de
Joshua Litam <joshua_litam@yahoo.com>
2023-07-03 12:54:11 +0800

    keep up the hoop

---
+++ codewars/8-kyu/hoopCount.js
@@ -0,0 +1,15 @@
+/*codewar challenge for the day link: https://www.codewars.com/kata/57eadb7ecd143f4c9c0000a3/train/javascript
+8 Kyu: Abbreviate a Two Word Name
+
+Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+The output should be two capital letters with a dot separating them.
+
+It should look like this:
+
+Sam Harris => S.H
+
+patrick feeney => P.F
+*/
+
+const abbrevName = str => str.split(" ").map(x => x[0].toUpperCase()).join(".")
