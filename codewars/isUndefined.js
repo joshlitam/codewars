3abf44464d0ea65cbd893fa62d256eaeba6f1160
Joshua Litam <joshua_litam@yahoo.com>
2023-07-28 15:34:31 +0800

    is undefined

---
+++ codewars/isUndefined.js
@@ -0,0 +1,11 @@
+/*codewar challenge for the day link: https://www.codewars.com/kata/564a6486c2070faee6000011/train/javascript
+7 kyu: isUndefined
+Is Undefined?
+Help Timmy remove undefined values from his array, you are modifing a callback function which he uses on each element on his array.
+Task
+Your task is to finish the isUndefined method which returns true if the value is undefined :)
+*/
+
+function isUndefined(value) {
+  return value == undefined
+}
