2e88f114d7b42c1869b46c5e9e792328217042b3
Joshua Litam <joshua_litam@yahoo.com>
2023-08-04 19:35:15 +0800

    a promise is a promise

---
+++ codewars/promiseHelloWorld.js
@@ -0,0 +1,11 @@
+/*codewar challenge for the day link: https://www.codewars.com/kata/56a75b91688b49ad94000015/train/javascript
+7 kyu: A promise is a promise
+Create a resolved javascript Promise that will return 'Hello World!'.
+*/
+
+function promiseHelloWorld() {
+  // replace this nonsense with your Promise
+  return new Promise((resolve, reject) => {
+    resolve("Hello World!")
+  })
+}
