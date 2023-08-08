8d8518542957e38f3ad1a20aa04dedcfd0c80687
Joshua Litam <joshua_litam@yahoo.com>
2023-08-08 18:54:37 +0800

    reverse list

---
+++ codewars/7-kyu/reverseList.js
@@ -0,0 +1,8 @@
+/*codewar challenge for the day link: https://www.codewars.com/kata/57a04da9e298a7ee43000111/train/javascript
+7 kyu: Reverse List
+Write reverseList function that simply reverses lists. which returns true/True/.
+*/
+
+function reverseList(arr) {
+  return arr.reverse()
+}
