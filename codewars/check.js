a381bd3f3757a8ea84d22fcadbbdc835980e7dee
Joshua Litam <joshua_litam@yahoo.com>
2023-08-11 09:54:44 +0800

    no loops allowed 2 - you only need one

---
+++ codewars/check.js
@@ -0,0 +1,17 @@
+/*codewar challenge for the day link: https://www.codewars.com/kata/57cc40b2f8392dbf2a0003ce/train/javascript
+8 Kyu: No Loops 2 - You only need one
+
+*** No Loops Allowed ***
+
+You will be given an array a and a value x. All you need to do is check whether the provided array contains the value, without using a loop.
+
+Array can contain numbers or strings. x can be either. Return true if the array contains the value, false if not. With strings you will need to account for case.
+
+Looking for more, loop-restrained fun? Check out the other kata in the series:
+
+No Loops 1 - Small enough?
+*/
+
+function check(a,x) {
+    return a.indexOf(x) >= 0
+  };
