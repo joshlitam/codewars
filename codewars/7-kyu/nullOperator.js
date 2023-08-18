701ce38cf9c008991efaa1425bee36cba162ebe3
Joshua Litam <joshua_litam@yahoo.com>
2023-08-18 08:59:10 +0800

    null-conditional operator

--- codewars/7-kyu/nullOperator.js
+++ codewars/7-kyu/nullOperator.js
@@ -1,18 +1,25 @@
-/*codewar challenge for the day link: https://www.codewars.com/kata/56a75b91688b49ad94000015/train/javascript
-7 kyu: Excessively Abundant Numbers
-An abundant number or excessive number is a number for which the sum of its proper divisors is greater than the number itself.
+/*codewar challenge for the day link: https://www.codewars.com/kata/55bf6b640e7ec0138e000071/train/javascript
+7 kyu: Null-Conditional Operator (C# 6.0)
+In ECMAScript 2020, there is a new operator ?. called the optional chaining operator. This operator is available from Node.js v14 onwards.
 
-The integer 12 is the first abundant number. Its proper divisors are 1, 2, 3, 4 and 6 for a total of 16 (> 12).
+It's nothing revolutionary - just an example of syntactic sugar but a pretty neat one.
 
-Derive function abundantNumber(num)/abundant_number(num) which returns true/True/.true. if num is abundant, false/False/.false. if not.
+Use case
+Imagine you have a Car, the car has an Engine, the engine has a Gearbox and the gearbox has some property you are interested in, for example numberOfGears. Now, the car could be from junkyard and the gearbox or even the engine could be missing (null).
+
+The name of each property is the name of the class it contains, converted to the appropriate case, such that if all components are present the final property could be accessed by:
+
+Car.engine.gearbox.numberOfGears
+
+How to determine the number of gears in a car?
+
+Instructions
+Complete the method called getNumberOfGears being added to the prototype of the Car class. This method will return the value of the numberOfGears property of the gearbox or null if the gearbox (or anything else) is missing.
+
+Constraints
+Find some information about the ?. and ?? operators and try to write the method without using if or the ternary operator ?:.
 */
 
-function abundantNumber(num) {
-    let divisors = [];
-    for (let i = 1; i < num - 1; i++) {
-      if (num % i == 0) {
-        divisors.push(i);
-      }
-    }
-    return divisors.reduce((a, b) => a + b, 0) > num;
-  }
+Car.prototype.getNumberOfGears = function getNumberOfGears() {
+  return this.engine?.gearbox?.numberOfGears ?? null
+}
